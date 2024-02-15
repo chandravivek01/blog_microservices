0498712550925f9ef67ea19fb0eb61e6c7a99b90
Vivek Chandra Sardar <34533136+chandravivek01@users.noreply.github.com>
2024-02-15 09:57:41 +0530

    Add files via upload

---
+++ src/PostCreate.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import axios  from 'axios';
+
+const PostCreate = () => {
+
+    const onSubmit = async ( event ) => {
+        
+        event.preventDefault();
+        await axios.post('http://localhost:4000/posts', {
+            title
+        });
+        setTitle('');
+    };
+
+    const [title, setTitle] = useState('');
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input value={title} onChange={ e => setTitle(e.target.value)} type="text" className='form-control' />
+                </div>
+                <button className="btn btn-primary mt-2">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default PostCreate
